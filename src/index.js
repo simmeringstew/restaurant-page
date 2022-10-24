5999de551b1d405b66d00ebc6d4115bb40a035d6
simmeringstew <dev.95a2e@simplelogin.com>
2022-10-24 16:42:54 -0400

    Added images and a font

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import "./style.css";
+import Eggs from "./eggs.jpeg";
+import Pancakes from "./pancakes.jpeg";
+import Pizza from "./pizza.jpeg";
+import Salmon from "./salmon.jpeg";
+
 console.log("Hello");
