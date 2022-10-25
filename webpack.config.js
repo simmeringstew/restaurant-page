659b5cea370385847a493296be0b78ca4f7042d6
simmeringstew <dev.95a2e@simplelogin.com>
2022-10-25 13:11:13 -0400

    All js files added to entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    remove: "./src/remove.js",
+    createHome: "./src/createHome.js",
+    createMenu: "./src/createMenu.js",
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -18,6 +23,5 @@ module.exports = {
         type: 'asset/resource',
       },
     ],
-    mode: "none",
   },
 };
