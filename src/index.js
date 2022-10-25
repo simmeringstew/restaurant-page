0d42e8b8204e132f41fe6738a1250dfcaa16ac35
simmeringstew <dev.95a2e@simplelogin.com>
2022-10-25 13:10:47 -0400

    Main js file for controlling what happens when the user changes page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
+import "./meyer-reset.css";
 import "./style.css";
-import Eggs from "./eggs.jpeg";
-import Pancakes from "./pancakes.jpeg";
-import Pizza from "./pizza.jpeg";
-import Salmon from "./salmon.jpeg";
+import removeMain from "./remove.js";
+import createHome from "./createHome.js";
+import createMenu from "./createMenu.js";
+import createContact from "./createContact.js";
 
-console.log("Hello");
+// loads home page on first website load
+
+window.onload = createHome;
+
+// selectors for the navigation
+
+const home = document.querySelector(".home");
+home.addEventListener("click", () => {
+    removeMain();
+    createHome();
+});
+
+const menu = document.querySelector(".menu");
+menu.addEventListener("click", () => {
+    removeMain();
+    createMenu();
+});
+
+const contact = document.querySelector(".contact");
+contact.addEventListener("click", () => {
+    removeMain();
+    createContact();
+});
